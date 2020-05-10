0760ba931be0347f8024a030ee9d8e9f3f0aeedc
d-otis <dan@dan-foley.com>
2020-05-10 18:57:54 -0400

    create print methods via macros for integer attrs

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,13 +1,36 @@
 class State
 
 	attr_accessor 	:state, :name, :twitter, :notes, :covid19SiteOld, 
-					:covid19Site, :covid19SiteSecondary, :positive, 
-					:negative, :dataQualityGrade, :hospitalizedCurrently,
-					:hosiitalizedCumulative, :inIcuCurrently, :inIcuCumulative,
+					:covid19Site, :covid19SiteSecondary, :screenshots, :dateModified, :dateChecked, 
+					:positive, :negative, :dataQualityGrade, :hospitalizedCurrently,
+					:hospitalizedCumulative, :inIcuCurrently, :inIcuCumulative,
 					:onVentilatorCurrently, :onVentilatorCumulative, :recovered,
-					:death, :totalTestResults, :dateModified, :dateChecked, :screenshots
+					:death, :totalTestResults
 
 	@@all = []
+	@@int_attrs = ["positive",
+					 "negative",
+					 "recovered",
+					 "death",
+					 "totalTestResults",
+					 "dataQualityGrade",
+					 "hospitalizedCurrently",
+					 "hospitalizedCumulative",
+					 "inIcuCurrently",
+					 "inIcuCumulative",
+					 "onVentilatorCurrently",
+					 "onVentilatorCumulative"]
+
+	def integer_attrs
+		self.instance_variables[7...-3].collect{|attr| attr.to_s[1..-1]}
+		# returns array of all integer attrs
+	end
+
+	@@int_attrs.each do |attr| 
+		define_method("print_#{attr}") do 
+			format_num(instance_variable_get("@#{attr}"))
+		end
+	end
 
 	def initialize(hash)
 		hash.each {|k, v| self.send("#{k}=", v)}
@@ -30,10 +53,6 @@ class State
 		end
 	end
 
-	def print_pos
-		format_num(self.positive)
-	end
-
 	def print_twitter
 		if self.has_twitter?
 			"<a href=\"https://www.twitter.com/#{self.twitter[1..-1]}\" target=\"_blank\"><i class=\"fab fa-twitter\"></i></a>"
@@ -44,71 +63,41 @@ class State
 		self.twitter.nil? ? false : true
 	end
 
-	def print_neg
-		format_num(self.negative)
-	end
-
-	def print_tests
-		format_num(self.totalTestResults)
-	end
-
-	def print_recovered
-		format_num(self.recovered)
-	end
-
-	def print_death
-		format_num(self.death)
-	end
-
-	def print_icu_current
-		format_num(self.inIcuCurrently)
-	end
-
-	def print_hospitalized_current
-		format_num(self.hospitalizedCurrently)
-	end
-
-	def print_vent_current
-		format_num(self.onVentilatorCurrently)
-	end
-
 	def print_time_checked
 		time = Time.parse(self.dateChecked)
-		"Last checked: #{time.strftime("%D")} @ #{time.strftime("%I:%M %p")}"
+		"Last checked: #{time.strftime("%D")} @ #{time.strftime("%I:%M %p")} UTC"
 	end
 
 	def print_time_modified
 		time = Time.parse(self.dateModified)
-		"Last modified: #{time.strftime("%D")} @ #{time.strftime("%I:%M %p")}"
+		"Last modified: #{time.strftime("%D")} @ #{time.strftime("%I:%M %p")} UTC"
 	end
 
 	def print_grade
 		grade = self.dataQualityGrade
-		if ["A+", "A"].include?(grade)
-			"<span class='badge badge-pill badge-success'>#{grade}</span>"
-		elsif grade == "B"
-			"<span class='badge badge-pill badge-primary'>#{grade}</span>"
-		elsif grade == "C"
-			"<span class='badge badge-pill badge-warning'>#{grade}</span>"
+		case grade
+		when "A+", "A"
+			css = "success"
+		when "B"
+			css = "primary"
+		when "C"
+			css = "warning"
 		else
-			"<span class='badge badge-pill badge-danger'>#{grade}</span>"
+			css = "danger"
 		end
+		"<span class='badge badge-pill badge-#{css}'>#{grade}</span>"
 	end
 
 	def format_num(num)
 		if !num.nil?
 			num.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
 		else
-			"Not provided."
+			"<small class='text-muted'><em>Not provided.</em></small>"
 		end
 	end
 
 	def self.find(abbrev)
 		self.all.find {|state| state.state == abbrev}
-	end
-
-	def update_state(hash)
-		binding.pry
 	end
 
 	def self.first
