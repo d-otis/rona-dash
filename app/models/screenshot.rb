e9518879de72ed569d8ac405ec5e87e22826e3d5
d-otis <dan@dan-foley.com>
2020-05-10 11:14:46 -0400

    build initializer for Screenshot class

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,8 +1,20 @@
 class Screenshot
 
-	attr_accessor :state
+	attr_accessor :state, :url, :dateChecked, :secondary, :date, :size
 
 	@@all = []
+
+	def initialize(hash)
+		hash.each do |k, v| 
+			if k != "state"
+				self.send("#{k}=", v)
+			else
+				@state = State.find(v)
+				@state.screenshots << self
+			end
+		end
+		save
+	end
 
 	def save
 		self.class.all << self
